864427721213185001f90efcf08ca37ca7709965
Aidar <aidarsarvartdinov2004@gmail.com>
2025-08-29 17:46:08 +0300

    Implement hashMap put and get methods

---
+++ src/main/java/com/aidar/ds/hashMap/Main.java
@@ -0,0 +1,78 @@
+package com.aidar.ds.hashMap;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new MyHashMap<String, Integer>(10);
+        map.put("One", 1);
+        map.put("AAA", 3);
+        System.out.println(map.get("One"));
+    }
+}
+
+interface Map<K, V> {
+    void put(K key, V value);
+    V get(K key);
+}
+
+class KeyValuePair<K, V> {
+    K key;
+    V value;
+
+    public KeyValuePair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class MyHashMap<K, V> implements Map<K, V> {
+    private List<KeyValuePair<K, V>>[] hashTable;
+    private int mapSize;
+    private int capacity;
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap(int capacity) {
+        this.capacity = capacity;
+        hashTable = new List[capacity];
+        for (int i = 0; i < capacity; i++) {
+            hashTable[i] = new LinkedList<>();
+        }
+    }
+
+    @Override
+    public void put(K key, V value) {
+        KeyValuePair<K, V> entry = getEntry(key);
+        if (entry != null) {
+            entry.value = value;
+        } else {
+            int hash = getHash(key);
+            hashTable[hash].add(new KeyValuePair<K, V>(key, value));
+            mapSize++;
+        }
+    }
+
+    @Override
+    public V get(K key) {
+        KeyValuePair<K, V> entry = getEntry(key);
+        if (entry != null) {
+            return entry.value;
+        }
+        return null;
+    }
+
+    private KeyValuePair<K, V> getEntry(K key) {
+        int hash = getHash(key);
+        for (KeyValuePair<K, V> entry : hashTable[hash]) {
+            if (key.equals(entry.key)) {
+                return entry;
+            }
+        }
+        return null;
+    }
+
+    private int getHash(K key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+}
